68b3b4df471642be3869d3fd3124493a00fadf59
Gubelin Pablo <122590070+pabflow@users.noreply.github.com>
2025-06-05 17:22:16 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,1 +1,22 @@
+<?php
+header('Content-Type: application/json');
+if (isset($_FILES['uploadfile'])) {
+  $file = $_FILES['uploadfile'];
+  $uploadDir = __DIR__ . "/public/uploads/";
+
+  if (!is_dir($uploadDir)) {
+    mkdir($uploadDir, 0755, true);
+  }
+
+  $targetPath = $uploadDir . basename($file["name"]);
+  if (move_uploaded_file($file["tmp_name"], $targetPath)) {
+    $fileUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/uploads/' . basename($file["name"]);
+    echo json_encode(["fileUrl" => $fileUrl]);
+  } else {
+    echo json_encode(["error" => "Upload failed"]);
+  }
+} else {
+  echo json_encode(["error" => "No file received"]);
+}
+?>
 
